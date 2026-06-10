69ff9e9d915be07cfb5055ba3ed62330502009c9
Abhi <abhishekrajpurohit2805@gmail.com>
2026-06-10 15:34:41 +0530

    Initial React + Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React With Vite</h1>
+      <p> Welcome to my Social App!</p>
+    </div>
+  )
+}
+
+export default App
